ddc8fa3d1557772636b36df3eb7ae95b72bd8a20
Paige Rust <pmrust@wpi.edu>
2025-10-21 10:11:32 -0400

    MATLAB-FlightGear configuration

--- MATLAB/Animation/fg_sim.m
+++ MATLAB/Animation/fg_sim.m
@@ -8,7 +8,7 @@ h.TimeseriesSourceType = 'Timeseries';
 h.TimeseriesSource = ts;
 
 h.FlightGearBaseDirectory = '/Applications/FlightGear.app';
-h.GeometryModelName = 'Animation/Parachutist';
+h.GeometryModelName = 'Parachutist';
 h.DestinationIpAddress = '127.0.0.1';
 h.DestinationPort = '5502';
 
@@ -23,11 +23,36 @@ h.InstallScenery = true;
 h.DisableShaders = true;
 h.TimeScaling = 5;
 
+setenv('FG_ROOT', '/Applications/FlightGear.app/Contents/Resources/data');
+
+h.OutputFileName = 'runfg.sh';
+
 get(h)
 
 GenerateRunScript(h)
 
-system('runfg.bat &');
+% --- Patch the launcher to use the correct executable and FG_ROOT ---
+fid = fopen('runfg.sh','r');
+txt = fread(fid,'*char')';
+fclose(fid);
 
-play(h)
+% Replace the broken fgfs line with the correct one
+txt = regexprep(txt, '\./\.\./MacOS/fgfs', ...
+    '/Applications/FlightGear.app/Contents/MacOS/FlightGear');
+
+% Add FG_ROOT if it isn't already there
+if ~contains(txt, 'export FG_ROOT')
+    txt = sprintf('#!/bin/zsh\nexport FG_ROOT="/Applications/FlightGear.app/Contents/Resources/data"\n%s', txt);
+end
+
+fid = fopen('runfg.sh','w');
+fwrite(fid, txt);
+fclose(fid);
+
+% --------------------------------------------------------------
+
+system('bash runfg.sh &');
+pause(5);
+play(h);
+
 end
