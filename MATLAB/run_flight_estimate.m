fca2cc5536bb45376aa210ccb9d8634cd1604cf5
Will Scheirey <williamscheirey@gmail.com>
2026-02-16 12:15:51 -0500

    added total weight and dz alt

--- MATLAB/run_flight_estimate.m
+++ MATLAB/run_flight_estimate.m
@@ -5,7 +5,7 @@ addpath(genpath("weather"));
 addpath(genpath("haars_data"));
 
 parent_dir = "haars_data";
-drop_dir = "DN153_Lt1_n16_08052025_Inside";
+drop_dir = "DN172_Lt3_n12_08072025_side_2";
 full_dir = fullfile(parent_dir, drop_dir);
 
 %% Get Drop Parameters
