dd257beabdf2fbb9a987b6a102fd2a9ac682922e
Will Scheirey <williamscheirey@gmail.com>
2026-03-15 15:00:34 -0400

    added better documentation for get_drop_info

--- MATLAB/Data_Processing/Data_Handling/get_drop_info.m
+++ MATLAB/Data_Processing/Data_Handling/get_drop_info.m
@@ -28,14 +28,22 @@ function drop_info = get_drop_info(data_accel, data_gyro, data_mag, data_gps, da
 %       .time_drop : Timestamp when the drop occurs
 %       .time_land : Timestamp when the drop ends (landing)
 
+% Set up parameters
+% Average 10 points
 accel_mean_window = 10;
 accel_norm = movmean(vecnorm(data_accel.data, 2, 2), accel_mean_window, 1);
-accel_drop = 15;
-step_back = 2;
 
+% Assume drop is when accel > 15 m/s^2
+accel_drop = 15;
+% Assuming landing is when vel < 2 m/s
 vel_land = 2;
+
+% Take 2 seconds before and 2 seconds after identified drop and landing,
+% just to be safe.
+step_back = 2;
 step_forward = 2;
 
+% Find when the drop occurred
 drop_idx = find(accel_norm > accel_drop, 1);
 
 if isempty(drop_idx)
@@ -43,8 +51,10 @@ if isempty(drop_idx)
     return
 end
 
+% Calculate the time
 drop_time = data_accel.time(drop_idx) - step_back;
 
+% Trim the data for identification of landing
 trim_fcn = @(data) data(find(data.time > drop_time, 1):end, :);
 
 data_accel = trim_fcn(data_accel);
@@ -55,15 +65,19 @@ data_baro = trim_fcn(data_baro);
 data_gps_vel = trim_fcn(data_gps_vel);
 data_flysight_gps.GNSS = trim_fcn(data_flysight_gps.GNSS);
 
+% Find when landing occurred
 vel_norm = movmean(vecnorm(data_gps_vel.data, 2, 2), accel_mean_window, 1);
 land_idx = find(vel_norm < vel_land, 1);
-land_time = data_gps_vel.time(land_idx) + step_forward;
 
 if isempty(land_idx)
     drop_info = [];
     return
 end
 
+% Calculate the time
+land_time = data_gps_vel.time(land_idx) + step_forward;
+
+% Trim the data
 trim_fcn = @(data) data(1:find(data.time > land_time, 1), :);
 
 data_accel = trim_fcn(data_accel);
@@ -74,7 +88,7 @@ data_baro = trim_fcn(data_baro);
 data_gps_vel = trim_fcn(data_gps_vel);
 data_flysight_gps.GNSS = trim_fcn(data_flysight_gps.GNSS);
 
-
+% Generate the output struct
 drop_info = struct( ...
     'gps_drop', [data_gps.data(1,1:2), data_baro.data(1)], ...
     'vel_drop', data_gps_vel.data(1,:),...
