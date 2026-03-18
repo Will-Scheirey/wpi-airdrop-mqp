0982473195acaa19dabace91cf4de499ea69ccf4
Will Scheirey <williamscheirey@gmail.com>
2026-03-18 12:39:55 -0400

    Update Airdrop_Filter.m

--- MATLAB/Estimation/Filters/Airdrop_Filter.m
+++ MATLAB/Estimation/Filters/Airdrop_Filter.m
@@ -30,7 +30,6 @@ classdef Airdrop_Filter < Abstract_Filter
         % P_CURR The current state covariance estimate matrix
         P_curr
 
-        H
         % LAST_U The most recent inputs for the system
         last_u
         % M_REF_I The reference magnetic field vector
@@ -215,7 +214,8 @@ classdef Airdrop_Filter < Abstract_Filter
                 q_meas = quat_from_acc_mag(accel_meas_corr, mag_meas);
             end
 
-            C_bi = body2enu_rotm(q_meas);     % body -> inertial (b2i)
+            % Create our reference magnetic field vectr
+            C_bi = body2enu_rotm(q_meas);
             m_b0 = mag_meas / norm(mag_meas);  % measured mag in body
 
             obj.m_ref_i = C_bi * m_b0;         % inertial reference mag
@@ -541,13 +541,16 @@ classdef Airdrop_Filter < Abstract_Filter
 
             % Generate the prediction and its jacobian
             y_pred = obj.h(meas_idx);
-            my_H = obj.h_jacobian_states(meas_idx);
+            H = obj.h_jacobian_states(meas_idx);
 
             % Extract the right measurement nosie covariance
             range = obj.measurement_ranges{meas_idx};
             R_meas = obj.R(range, range);
 
-            [innovation, K, S] = obj.update_impl(y, y_pred, my_H, R_meas);
+            % Run the update implementation
+            [innovation, K, S] = obj.update_impl(y, y_pred, H, R_meas);
+
+            % Normalize the quaternion 
             obj.normalize_quat();
         end
 
