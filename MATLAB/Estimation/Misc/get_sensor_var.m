6275cfe96cd8b1a91d26e87e69739f060234fbae
Will Scheirey <williamscheirey@gmail.com>
2026-03-17 23:25:29 -0400

    added documentation for get_sensor_var

--- MATLAB/Estimation/Misc/get_sensor_var.m
+++ MATLAB/Estimation/Misc/get_sensor_var.m
@@ -1,4 +1,22 @@
 function [var_accel, var_gyro, var_gps, var_mag, var_baro] = get_sensor_var(data_stationary)
+    % GET_SENSOR_VAR Calculates sensor variance from measurements
+    %   This function is used for sensor calibration before flight,
+    %   calculating variance per sensor axis
+    %   
+    % INPUTS:
+    %   data_stationary : Measurements during the calibration period
+    %       .data_accel
+    %       .data_gyro
+    %       .data_gps
+    %       .data_mag
+    %       .data_baro
+    % OUTPUTS:
+    %   var_accel : Calculated accelerometer variance
+    %   var_gyro  : Calculated gyroscope variance
+    %   var_gps   : Calculated GPS position variance
+    %   var_mag   : Calculated magnetometer variance
+    %   var_baro  : Calculated barometer
+
     accel = data_stationary.data_accel;
     gyro  = data_stationary.data_gyro;
     gps   = data_stationary.data_gps;
