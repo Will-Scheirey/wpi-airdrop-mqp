64442957a6d5d71ab4cf84bae78830917ebd7fa8
Will Scheirey <williamscheirey@gmail.com>
2026-03-18 22:39:36 -0400

    added documentation to std_dev_from_noise

--- MATLAB/Modeling/Sensors/Noise/std_dev_from_noise.m
+++ MATLAB/Modeling/Sensors/Noise/std_dev_from_noise.m
@@ -1,3 +1,11 @@
 function std_dev = std_dev_from_noise(noise_density, hz)
+    % STD_DEV_FROM_NOISE Calculates standard deviation from noise density
+    % 
+    % INPUTS:
+    %   noise_density : Noise density in units / [sqrt(Hz)]
+    %   hz            : Polling frequency
+    %
+    % OUTPUTS:
+    %   std_dev : Calculated standard deviation
     std_dev = noise_density * sqrt(hz);
 end
