27990226f57f41a1e1c09961f62a913a6b03110e
Will Scheirey <williamscheirey@gmail.com>
2026-03-18 22:37:41 -0400

    added documentation to sensor_noise_white

--- MATLAB/Modeling/Sensors/Noise/sensor_noise_white.m
+++ MATLAB/Modeling/Sensors/Noise/sensor_noise_white.m
@@ -1,8 +1,15 @@
-function measurements = sensor_noise_white(y, std_dev)
-%SENSOR_EMULATOR Summary of this function goes here
-%   Detailed explanation goes here
+function y_out = sensor_noise_white(y, std_dev)
+% SENSOR_NOISE_WHITE Applies white sensor noise to values
+%   This function applies white gaussian noise to values with a specified
+%   standard deviation
+%
+% INPUTS:
+%   y : The un-noised values
+%   std_dev : The white noise standard deviation
+%
+% OUTPUTS:
+%   y_out : The noised values
 
     noise = std_dev * randn(size(y));
-
-    measurements = y + noise;
+    y_out = y + noise;
 end
